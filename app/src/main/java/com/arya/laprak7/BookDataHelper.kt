1ba3a6a764e5109c94ea6dbbd5f75344ca667a98
CreateLunary <ghimi.software@gmail.com>
2025-12-02 15:33:39 +0700

    Initial Commit

--- app/src/main/java/com/arya/laprak7/BookDataHelper.kt
+++ app/src/main/java/com/arya/laprak7/BookDataHelper.kt
@@ -8,9 +8,9 @@ object BookDataHelper {
             2 -> "https://upload.wikimedia.org/wikipedia/en/a/a0/Harry_Potter_and_the_Prisoner_of_Azkaban.jpg"
             3 -> "https://upload.wikimedia.org/wikipedia/en/b/b6/Harry_Potter_and_the_Goblet_of_Fire_cover.png"
             4 -> "https://upload.wikimedia.org/wikipedia/en/7/70/Harry_Potter_and_the_Order_of_the_Phoenix.jpg"
-            5 -> "https://upload.wikimedia.org/wikipedia/en/a/a9/Harry_Potter_and_the_Half-Blood_Prince.jpg"
+            5 -> "https://upload.wikimedia.org/wikipedia/en/b/b5/Harry_Potter_and_the_Half-Blood_Prince_cover.png"
             6 -> "https://upload.wikimedia.org/wikipedia/en/a/a9/Harry_Potter_and_the_Deathly_Hallows.jpg"
-            7 -> "https://upload.wikimedia.org/wikipedia/en/thumb/6/6e/Harry_Potter_and_the_Cursed_Child_Special_Rehearsal_Edition_Book_Cover.jpg/220px-Harry_Potter_and_the_Cursed_Child_Special_Rehearsal_Edition_Book_Cover.jpg"
+            7 -> "https://upload.wikimedia.org/wikipedia/en/8/87/Cursed_Child_new_poster.jpg"
             else -> "https://via.placeholder.com/150"
         }
     }
